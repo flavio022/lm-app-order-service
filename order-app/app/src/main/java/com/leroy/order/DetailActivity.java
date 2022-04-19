0fb04afcf59784c400422e18f0ffa8b22751b472
Flavio <flavio.danilo022@gmail.com>
2022-04-19 00:34:52 -0300

    realizanod chamada para finalizar os

--- order-app/app/src/main/java/com/leroy/order/DetailActivity.java
+++ order-app/app/src/main/java/com/leroy/order/DetailActivity.java
@@ -3,10 +3,21 @@ package com.leroy.order;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -33,6 +44,7 @@ public class DetailActivity extends AppCompatActivity {
         TextView nomeEmpresaNome = findViewById(R.id.nomeEmpresa_nome);
         TextView nomeEmpresa = findViewById(R.id.nomeEmpresa);
 
+        Button botao_enviar = findViewById(R.id.button_atendimento);
 
         Bundle bundle = getIntent().getExtras();
 
@@ -62,5 +74,55 @@ public class DetailActivity extends AppCompatActivity {
         nomeEmpresaNome.setText("Empresa:");
         nomeEmpresa.setText(mNomeEmpresa);
 
+        botao_enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendWorkPostRequest();
+            }
+        });
+
     }
+
+
+    private void sendWorkPostRequest() {
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://192.168.0.10:8081/ordemDeServico";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Toast.makeText(DetailActivity.this, "Response is: " + response.substring(0,500), Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(DetailActivity.this, "That didn't work!", Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("endereco", "Rua dene");
+
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("Content-Type","application/json");
+                return params;
+            }
+        };
+        queue.add(stringRequest);
+
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+}
+
+
 }
